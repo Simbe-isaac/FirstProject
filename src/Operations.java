ae3bc070239445793f4fdfcf950cd6f8f58668d2
USER <USER@10.113.7.239>
2019-10-30 13:51:24 -0700

    This is my first Java Project

---
+++ src/Operations.java
@@ -0,0 +1,12 @@
+
+public class Operations {
+    
+    public static void main(String[]args){
+        Boarding ab =new Boarding("SIMBE","MALE","BSCS",1998,320000,430000);
+        Boarding bc =new Boarding("SIMBE","MALE","BSCS",1998,320000,430000);
+        
+       ab.Payment();
+       
+       
+    } 
+}
